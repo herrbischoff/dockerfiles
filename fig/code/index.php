2cf82ab15d044e6f00309b0cf95dbaac267f0ccf
Marcel Bischoff <marcel@herrbischoff.com>
2016-04-04 01:52:22 +0200

    Update dockerfiles to something more usable

--- fig/code/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "This is the MySQL container info:";
-    echo "<br>";
-    echo "<pre>";
-    echo getenv("DB_1_PORT_3306_TCP_ADDR") . ":" . getenv("DB_1_PORT_3306_TCP_PORT");
-    echo "</pre>";
